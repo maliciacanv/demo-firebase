a345ba2ad404acd95ffd85799527cc1a0ac44132
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-05-01 12:26:36 -0500

    Se añadieron scripts de firebase en el html para la autenticación de usuarios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import { initRouterDemo } from './root/router.js'
+import { initRouterDemo } from './root/router.js';
 
 const init = () => {
-    const config = {
-        apiKey: "AIzaSyCXp9b4bxUDX6ulbYYSitQ8MyhoKXvtSVw",
-        authDomain: "fir-presentacion-gym.firebaseapp.com",
-        databaseURL: "https://fir-presentacion-gym.firebaseio.com",
-        projectId: "fir-presentacion-gym",
-        storageBucket: "fir-presentacion-gym.appspot.com",
-        messagingSenderId: "4848208798"
-      };
-firebase.initializeApp(config);
-initRouterDemo();
-}
+  const config = {
+    apiKey: 'AIzaSyCXp9b4bxUDX6ulbYYSitQ8MyhoKXvtSVw',
+    authDomain: 'fir-presentacion-gym.firebaseapp.com',
+    databaseURL: 'https://fir-presentacion-gym.firebaseio.com',
+    projectId: 'fir-presentacion-gym',
+    storageBucket: 'fir-presentacion-gym.appspot.com',
+    messagingSenderId: '4848208798'
+  };
+
+  firebase.initializeApp(config);
+
+  initRouterDemo();
+};
+
 window.onload = init;
